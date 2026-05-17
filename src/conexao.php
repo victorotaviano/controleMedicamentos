65fd10eb778362e5837a11d99337121a5f640d03
Maria Morena <mariasixmorena@gmail.com>
2026-05-17 11:53:42 -0300

    fix: ajuste na lógica de conexão com banco remoto

--- src/conexao.php
+++ src/conexao.php
@@ -2,22 +2,27 @@
 $db_url = getenv('DATABASE_URL');
 
 if ($db_url) {
+
     $dbopts = parse_url($db_url);
+    
     $host = $dbopts["host"];
-    $port = $dbopts["port"];
+
+    $port = isset($dbopts["port"]) ? $dbopts["port"] : "5432";
     $user = $dbopts["user"];
     $pass = $dbopts["pass"];
     $db   = ltrim($dbopts["path"], '/');
+    
+    
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db";
 } else {
-    $host = "localhost";
-    $port = "5432";
-    $db   = "controleMedicamentos";
+
+    $dsn = "pgsql:host=localhost;port=5432;dbname=controleMedicamentos";
     $user = "postgres";
-    $pass = "senha"; 
+    $pass = "sua_senha_local";
 }
 
 try {
-    $conexao = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $conexao = new PDO($dsn, $user, $pass);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     exit("Erro na conexão: " . $e->getMessage());
