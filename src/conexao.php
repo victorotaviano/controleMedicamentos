4e06be4239df920ade2104079d429bd3014137e6
Maria Morena <mariasixmorena@gmail.com>
2026-05-17 12:11:41 -0300

    Alterações para blueprint

--- src/conexao.php
+++ src/conexao.php
@@ -1,25 +1,20 @@
 <?php
-
 $db_url = getenv('DATABASE_URL');
 
 if ($db_url) {
-    
+    // O Render Blueprint envia como postgres://, o PHP precisa de pgsql:
     $dsn = str_replace('postgres://', 'pgsql:', $db_url);
-    
-   
     $user = null;
     $pass = null;
 } else {
-   
     $dsn = "pgsql:host=localhost;port=5432;dbname=controleMedicamentos";
     $user = "postgres";
-    $pass = "senha"; 
+    $pass = "senha";
 }
 
 try {
     $conexao = new PDO($dsn, $user, $pass);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    
     exit("Erro na conexão: " . $e->getMessage());
 }
