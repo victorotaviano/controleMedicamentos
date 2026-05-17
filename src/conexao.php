e2088e240a620ea8b38f2b8e1b2c078628fd748e
Maria Morena <mariasixmorena@gmail.com>
2026-05-17 11:55:43 -0300

    fix: correção  na lógica de conexão com banco remoto

--- src/conexao.php
+++ src/conexao.php
@@ -2,28 +2,28 @@
 $db_url = getenv('DATABASE_URL');
 
 if ($db_url) {
-
-    $dbopts = parse_url($db_url);
-    
-    $host = $dbopts["host"];
-
-    $port = isset($dbopts["port"]) ? $dbopts["port"] : "5432";
-    $user = $dbopts["user"];
-    $pass = $dbopts["pass"];
-    $db   = ltrim($dbopts["path"], '/');
-    
-    
-    $dsn = "pgsql:host=$host;port=$port;dbname=$db";
+    if (preg_match('/postgres:\/\/(.*?):(.*?)\@(.*?)(?::(\d+))?\/(.*)/', $db_url, $matches)) {
+        $user = $matches[1];
+        $pass = $matches[2];
+        $host = $matches[3];
+        $port = (!empty($matches[4])) ? $matches[4] : "5432";
+        $db   = explode('?', $matches[5])[0];
+        
+        $dsn = "pgsql:host=$host;port=$port;dbname=$db";
+    } else {
+        exit("Erro: Formato de DATABASE_URL inválido.");
+    }
 } else {
 
     $dsn = "pgsql:host=localhost;port=5432;dbname=controleMedicamentos";
     $user = "postgres";
-    $pass = "sua_senha_local";
+    $pass = "senha"; 
 }
 
 try {
     $conexao = new PDO($dsn, $user, $pass);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
+   
     exit("Erro na conexão: " . $e->getMessage());
 }
