0a81751277a57777e7af8e25eaa77aa0f05dd6d9
Maria Morena <mariasixmorena@gmail.com>
2026-05-17 11:59:47 -0300

    fix: simplificando conexao com DSN direto

--- src/conexao.php
+++ src/conexao.php
@@ -1,20 +1,16 @@
 <?php
+
 $db_url = getenv('DATABASE_URL');
 
 if ($db_url) {
-    if (preg_match('/postgres:\/\/(.*?):(.*?)\@(.*?)(?::(\d+))?\/(.*)/', $db_url, $matches)) {
-        $user = $matches[1];
-        $pass = $matches[2];
-        $host = $matches[3];
-        $port = (!empty($matches[4])) ? $matches[4] : "5432";
-        $db   = explode('?', $matches[5])[0];
-        
-        $dsn = "pgsql:host=$host;port=$port;dbname=$db";
-    } else {
-        exit("Erro: Formato de DATABASE_URL inválido.");
-    }
+    
+    $dsn = str_replace('postgres://', 'pgsql:', $db_url);
+    
+   
+    $user = null;
+    $pass = null;
 } else {
-
+   
     $dsn = "pgsql:host=localhost;port=5432;dbname=controleMedicamentos";
     $user = "postgres";
     $pass = "senha"; 
@@ -24,6 +20,6 @@ try {
     $conexao = new PDO($dsn, $user, $pass);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-   
+    
     exit("Erro na conexão: " . $e->getMessage());
 }
